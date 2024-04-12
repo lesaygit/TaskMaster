20fa508d58962d16e1efe059c9dd261ce52c1a19
Yasel Glez Ortiz <lesaywd@gmail.com>
2024-04-11 22:10:17 -0300

    add task container

---
+++ src/app/Task/task-container/task-container.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-task-container',
+  standalone: true,
+  imports: [],
+  templateUrl: './task-container.component.html',
+  styleUrl: './task-container.component.css'
+})
+export class TaskContainerComponent {
+
+}
